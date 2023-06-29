1432f2580d923f7e9add3bb97707ca303b4ff45f
jujinhyeong <mhn03148@naver.com>
2023-06-29 20:57:51 +0900

    weather app with SwfitUI

---
+++ WeatherCloneApp/WeatherCloneApp/WeatherCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCloneAppApp.swift
+//  WeatherCloneApp
+//
+//  Created by 주진형 on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
